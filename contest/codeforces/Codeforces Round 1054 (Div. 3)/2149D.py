6582442a7b19e5546bd42e49850a3d93b352e63a
helltractor <1664309610@qq.com>
2025-09-26 12:32:50 +0800

    Add solutions for Codeforces Round 1054 (Div.3) problems A to F

---
+++ contest/codeforces/Codeforces Round 1054 (Div. 3)/2149D.py
@@ -0,0 +1,162 @@
+ImportType = InputType = ConstType = 1
+DecoratorType = FunctinoType = 1
+if ImportType:
+    import os, sys, random, threading
+    from random import randint, choice, shuffle
+    from copy import deepcopy
+    from io import BytesIO, IOBase
+    from types import GeneratorType
+    from functools import lru_cache, reduce
+    from bisect import bisect_left, bisect_right
+    from collections import Counter, defaultdict, deque
+    from itertools import accumulate, combinations, permutations
+    from heapq import heapify, heappop, heappush
+    from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+    from string import ascii_lowercase, ascii_uppercase, digits
+    from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf
+    from decimal import Decimal, getcontext
+    from sys import stdin, stdout, setrecursionlimit
+
+if InputType:
+
+    class FastIO(IOBase):
+        newlines = 0
+
+        def __init__(self, file):
+            self._fd = file.fileno()
+            self.buffer = BytesIO()
+            self.writable = "x" in file.mode or "r" not in file.mode
+            self.write = self.buffer.write if self.writable else None
+
+        def read(self):
+            while True:
+                b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                if not b:
+                    break
+                ptr = self.buffer.tell()
+                self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+            self.newlines = 0
+            return self.buffer.read()
+
+        def readline(self):
+            while self.newlines == 0:
+                b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                self.newlines = b.count(b"\n") + (not b)
+                ptr = self.buffer.tell()
+                self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+            self.newlines -= 1
+            return self.buffer.readline()
+
+        def flush(self):
+            if self.writable:
+                os.write(self._fd, self.buffer.getvalue())
+                self.buffer.truncate(0), self.buffer.seek(0)
+
+    class IOWrapper(IOBase):
+        def __init__(self, file):
+            self.buffer = FastIO(file)
+            self.flush = self.buffer.flush
+            self.writable = self.buffer.writable
+            self.write = lambda s: self.buffer.write(s.encode("ascii"))
+            self.read = lambda: self.buffer.read().decode("ascii")
+            self.readline = lambda: self.buffer.readline().decode("ascii")
+
+    BUFSIZE = 1 << 12
+    sys.stdin = IOWrapper(sys.stdin)
+    sys.stdout = IOWrapper(sys.stdout)
+    input = lambda: sys.stdin.readline().rstrip("\r\n")
+    I = lambda: input()
+    II = lambda: int(input())
+    MII = lambda: map(int, input().split())
+    LI = lambda: list(input())
+    LII = lambda: list(map(int, input().split()))
+    GMI = lambda: map(lambda x: int(x) - 1, input().split())
+    LGMI = lambda: list(map(lambda x: int(x) - 1, input().split()))
+
+if DecoratorType:
+
+    def bootstrap(f, stack=[]):
+        def wrappedfunc(*args, **kwargs):
+            if stack:
+                return f(*args, **kwargs)
+            else:
+                to = f(*args, **kwargs)
+                while True:
+                    if type(to) is GeneratorType:
+                        stack.append(to)
+                        to = next(to)
+                    else:
+                        stack.pop()
+                        if not stack:
+                            break
+                        to = stack[-1].send(to)
+                return to
+
+        return wrappedfunc
+
+
+if FunctinoType:
+    fmax = lambda x, y: x if x > y else y
+    fmin = lambda x, y: x if x < y else y
+
+if ConstType:
+    MOD1, MOD9 = 1000000007, 998244353
+    RD = random.randint(MOD1, MOD1 << 1)
+    D4 = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    D8 = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    Y, N = "Yes", "No"
+    A, B = "Alice", "Bob"
+
+
+# def helltractor():
+#     for _ in range(II()):
+#         n = II()
+#         a = LI()
+#         cnt = defaultdict(list)
+#         for i, c in enumerate(a, 1):
+#             cnt[c].append(i)
+#         m1 = len(cnt["a"])
+#         m2 = len(cnt["b"])
+#         pre1 = [0] * (m1 + 1)
+#         pre2 = [0] * (m2 + 1)
+#         for i in range(m1):
+#             pre1[i + 1] = pre1[i] + cnt["a"][i]
+#         for i in range(m2):
+#             pre2[i + 1] = pre2[i] + cnt["b"][i]
+#         ans = inf
+#         for i in range(m1 + 1):
+#             tmp = (
+#                 pre1[i]
+#                 - i * (i + 1) // 2
+#                 + (m1 - i) * (n + n - (m1 - i) + 1) // 2
+#                 - (pre1[-1] - pre1[i])
+#             )
+#             ans = fmin(ans, tmp)
+#         for i in range(m2 + 1):
+#             tmp = (
+#                 pre2[i]
+#                 - i * (i + 1) // 2
+#                 + (m2 - i) * (n + n - (m2 - i) + 1) // 2
+#                 - (pre2[-1] - pre2[i])
+#             )
+#             ans = fmin(ans, tmp)
+#         print(ans)
+
+
+def helltractor():
+    for _ in range(II()):
+        n = II()
+        a = LI()
+        ans = inf
+        for c in "ab":
+            b = [i for i, x in enumerate(a) if x == c]
+            for i in range(len(b)):
+                b[i] -= i
+            mid = b[len(b) // 2] if b else 0
+            res = sum(abs(i - mid) for i in b)
+            ans = fmin(ans, res)
+        print(ans)
+
+
+if __name__ == "__main__":
+    helltractor()
