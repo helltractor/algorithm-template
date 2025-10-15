b1373cce093b785b0a93b3c17c85ceb92c65b6db
helltractor <1664309610@qq.com>
2025-10-15 14:43:33 +0800

    Add solutions for Codeforces and Atcoder contests

---
+++ contest/atcoder/AtCoder Beginner Contest 426/abc426e.py
@@ -0,0 +1,174 @@
+ImportType = InputType = ConstType = 1
+from typing import Tuple
+DecoratorType = FunctinoType = 1
+if ImportType:
+    import os, sys, random, threading
+    from random import randint, choice, shuffle
+    from copy import deepcopy
+    from io import BytesIO, IOBase
+    from types import GeneratorType
+    from functools import lru_cache, reduce
+    from bisect import bisect_left, bisect_right
+    from collections import Counter, defaultdict, deque
+    from itertools import accumulate, combinations, permutations
+    from heapq import heapify, heappop, heappush
+    from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+    from string import ascii_lowercase, ascii_uppercase, digits
+    from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf
+    from decimal import Decimal, getcontext
+    from sys import stdin, stdout, setrecursionlimit
+
+if InputType:
+
+    class FastIO(IOBase):
+        newlines = 0
+
+        def __init__(self, file):
+            self._fd = file.fileno()
+            self.buffer = BytesIO()
+            self.writable = "x" in file.mode or "r" not in file.mode
+            self.write = self.buffer.write if self.writable else None
+
+        def read(self):
+            while True:
+                b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                if not b:
+                    break
+                ptr = self.buffer.tell()
+                self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+            self.newlines = 0
+            return self.buffer.read()
+
+        def readline(self):
+            while self.newlines == 0:
+                b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                self.newlines = b.count(b"\n") + (not b)
+                ptr = self.buffer.tell()
+                self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+            self.newlines -= 1
+            return self.buffer.readline()
+
+        def flush(self):
+            if self.writable:
+                os.write(self._fd, self.buffer.getvalue())
+                self.buffer.truncate(0), self.buffer.seek(0)
+
+    class IOWrapper(IOBase):
+        def __init__(self, file):
+            self.buffer = FastIO(file)
+            self.flush = self.buffer.flush
+            self.writable = self.buffer.writable
+            self.write = lambda s: self.buffer.write(s.encode("ascii"))
+            self.read = lambda: self.buffer.read().decode("ascii")
+            self.readline = lambda: self.buffer.readline().decode("ascii")
+
+    BUFSIZE = 1 << 12
+    sys.stdin = IOWrapper(sys.stdin)
+    sys.stdout = IOWrapper(sys.stdout)
+    input = lambda: sys.stdin.readline().rstrip("\r\n")
+    I = lambda: input()
+    II = lambda: int(input())
+    MII = lambda: map(int, input().split())
+    LI = lambda: list(input())
+    LII = lambda: list(map(int, input().split()))
+    GMI = lambda: map(lambda x: int(x) - 1, input().split())
+    LGMI = lambda: list(map(lambda x: int(x) - 1, input().split()))
+
+if DecoratorType:
+
+    def bootstrap(f, stack=[]):
+        def wrappedfunc(*args, **kwargs):
+            if stack:
+                return f(*args, **kwargs)
+            else:
+                to = f(*args, **kwargs)
+                while True:
+                    if type(to) is GeneratorType:
+                        stack.append(to)
+                        to = next(to)
+                    else:
+                        stack.pop()
+                        if not stack:
+                            break
+                        to = stack[-1].send(to)
+                return to
+
+        return wrappedfunc
+
+
+if FunctinoType:
+    fmax = lambda x, y: x if x > y else y
+    fmin = lambda x, y: x if x < y else y
+
+if ConstType:
+    MOD1, MOD9 = 1000000007, 998244353
+    RD = random.randint(MOD1, MOD1 << 1)
+    D4 = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    D8 = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    Y, N = "Yes", "No"
+    A, B = "Alice", "Bob"
+
+
+def distance_square(a: Tuple[int, int], b: Tuple[int, int]) -> int:
+    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+
+
+def subtract(a: Tuple[float, float], b: Tuple[float, float]) -> Tuple[float, float]:
+    return (a[0] - b[0], a[1] - b[1])
+
+
+def distance(a: Tuple[float, float], b: Tuple[float, float]) -> float:
+    d = subtract(a, b)
+    return sqrt(d[0] ** 2 + d[1] ** 2)
+
+
+def internal_division(
+    a: Tuple[float, float], b: Tuple[float, float], p: float
+) -> Tuple[float, float]:
+    x = a[0] + (b[0] - a[0]) * p
+    y = a[1] + (b[1] - a[1]) * p
+    return (x, y)
+
+
+NUM_ITERATION = 60
+
+
+def dist_segment_and_origin(a: Tuple[float, float], b: Tuple[float, float]) -> float:
+    def f(p: float) -> float:
+        return distance(internal_division(a, b, p), (0, 0))
+
+    l, r = 0.0, 1.0
+    for _ in range(NUM_ITERATION):
+        ml = (l * 2 + r) / 3
+        mr = (l + r * 2) / 3
+        if f(ml) < f(mr):
+            r = mr
+        else:
+            l = ml
+    return f(l)
+
+
+def helltractor():
+    for _ in range(II()):
+        a = [None, None]
+        b = [None, None]
+        for i in range(2):
+            ax, ay, bx, by = MII()
+            a[i] = (ax, ay)
+            b[i] = (bx, by)
+
+        if distance_square(a[0], b[0]) < distance_square(a[1], b[1]):
+            a[0], a[1] = a[1], a[0]
+            b[0], b[1] = b[1], b[0]
+        ta = (float(a[0][0]), float(a[0][1]))
+        tb = (float(b[0][0]), float(b[0][1]))
+        sa = (float(a[1][0]), float(a[1][1]))
+        sb = (float(b[1][0]), float(b[1][1]))
+        tm = internal_division(ta, tb, distance(sa, sb) / distance(ta, tb))
+        d1 = dist_segment_and_origin(subtract(ta, sa), subtract(tm, sb))
+        d2 = dist_segment_and_origin(subtract(tm, sb), subtract(tb, sb))
+        print(f"{min(d1, d2):.10f}")
+
+
+if __name__ == "__main__":
+    helltractor()
---
+++ contest/codeforces/Educational Codeforces Round 183 (Rated for Div. 2)/2145E.py
@@ -0,0 +1,362 @@
+ImportType = InputType = ConstType = 1
+DecoratorType = FunctinoType = 1
+if ImportType:
+    import os, sys, random, threading
+    from random import randint, choice, shuffle
+    from copy import deepcopy
+    from io import BytesIO, IOBase
+    from types import GeneratorType
+    from functools import lru_cache, reduce
+    from bisect import bisect_left, bisect_right
+    from collections import Counter, defaultdict, deque
+    from itertools import accumulate, combinations, permutations
+    from heapq import heapify, heappop, heappush
+    from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+    from string import ascii_lowercase, ascii_uppercase, digits
+    from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf
+    from decimal import Decimal, getcontext
+    from sys import stdin, stdout, setrecursionlimit
+    from operator import add
+    import typing
+
+if InputType:
+
+    class FastIO(IOBase):
+        newlines = 0
+
+        def __init__(self, file):
+            self._fd = file.fileno()
+            self.buffer = BytesIO()
+            self.writable = "x" in file.mode or "r" not in file.mode
+            self.write = self.buffer.write if self.writable else None
+
+        def read(self):
+            while True:
+                b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                if not b:
+                    break
+                ptr = self.buffer.tell()
+                self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+            self.newlines = 0
+            return self.buffer.read()
+
+        def readline(self):
+            while self.newlines == 0:
+                b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                self.newlines = b.count(b"\n") + (not b)
+                ptr = self.buffer.tell()
+                self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+            self.newlines -= 1
+            return self.buffer.readline()
+
+        def flush(self):
+            if self.writable:
+                os.write(self._fd, self.buffer.getvalue())
+                self.buffer.truncate(0), self.buffer.seek(0)
+
+    class IOWrapper(IOBase):
+        def __init__(self, file):
+            self.buffer = FastIO(file)
+            self.flush = self.buffer.flush
+            self.writable = self.buffer.writable
+            self.write = lambda s: self.buffer.write(s.encode("ascii"))
+            self.read = lambda: self.buffer.read().decode("ascii")
+            self.readline = lambda: self.buffer.readline().decode("ascii")
+
+    BUFSIZE = 1 << 12
+    sys.stdin = IOWrapper(sys.stdin)
+    sys.stdout = IOWrapper(sys.stdout)
+    input = lambda: sys.stdin.readline().rstrip("\r\n")
+    I = lambda: input()
+    II = lambda: int(input())
+    MII = lambda: map(int, input().split())
+    LI = lambda: list(input())
+    LII = lambda: list(map(int, input().split()))
+    GMI = lambda: map(lambda x: int(x) - 1, input().split())
+    LGMI = lambda: list(map(lambda x: int(x) - 1, input().split()))
+
+if DecoratorType:
+
+    def bootstrap(f, stack=[]):
+        def wrappedfunc(*args, **kwargs):
+            if stack:
+                return f(*args, **kwargs)
+            else:
+                to = f(*args, **kwargs)
+                while True:
+                    if type(to) is GeneratorType:
+                        stack.append(to)
+                        to = next(to)
+                    else:
+                        stack.pop()
+                        if not stack:
+                            break
+                        to = stack[-1].send(to)
+                return to
+
+        return wrappedfunc
+
+
+if FunctinoType:
+    fmax = lambda x, y: x if x > y else y
+    fmin = lambda x, y: x if x < y else y
+
+if ConstType:
+    MOD1, MOD9 = 1000000007, 998244353
+    RD = random.randint(MOD1, MOD1 << 1)
+    D4 = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    D8 = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    Y, N = "Yes", "No"
+    A, B = "Alice", "Bob"
+
+
+class LazySegmentTree:
+    __slots__ = [
+        "_op",
+        "_e",
+        "_mapping",
+        "_composition",
+        "_id",
+        "_n",
+        "_log",
+        "_size",
+        "_d",
+        "_lz",
+    ]
+
+    def __init__(
+        self,
+        op: typing.Callable[[typing.Any, typing.Any], typing.Any],
+        e: typing.Any,
+        mapping: typing.Callable[[typing.Any, typing.Any], typing.Any],
+        composition: typing.Callable[[typing.Any, typing.Any], typing.Any],
+        idlazy: typing.Any,
+        v: typing.Union[int, typing.List[typing.Any]],
+    ) -> None:
+        self._op = op
+        self._e = e
+        self._mapping = mapping
+        self._composition = composition
+        self._id = idlazy
+
+        if isinstance(v, int):
+            v = [e] * v
+
+        self._n = len(v)
+        self._log = (self._n - 1).bit_length()
+        self._size = 1 << self._log
+        self._d = [e] * (2 * self._size)
+        self._lz = [self._id] * self._size
+        for i in range(self._n):
+            self._d[self._size + i] = v[i]
+        for i in range(self._size - 1, 0, -1):
+            self._update(i)
+
+    def set(self, p: int, x: typing.Any) -> None:
+        assert 0 <= p < self._n
+
+        p += self._size
+        for i in range(self._log, 0, -1):
+            self._push(p >> i)
+        self._d[p] = x
+        for i in range(1, self._log + 1):
+            self._update(p >> i)
+
+    def get(self, p: int) -> typing.Any:
+        assert 0 <= p < self._n
+
+        p += self._size
+        for i in range(self._log, 0, -1):
+            self._push(p >> i)
+        return self._d[p]
+
+    def prod(self, left: int, right: int) -> typing.Any:
+        assert 0 <= left <= right <= self._n
+
+        if left == right:
+            return self._e
+
+        left += self._size
+        right += self._size
+
+        for i in range(self._log, 0, -1):
+            if ((left >> i) << i) != left:
+                self._push(left >> i)
+            if ((right >> i) << i) != right:
+                self._push((right - 1) >> i)
+
+        sml = self._e
+        smr = self._e
+        while left < right:
+            if left & 1:
+                sml = self._op(sml, self._d[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                smr = self._op(self._d[right], smr)
+            left >>= 1
+            right >>= 1
+
+        return self._op(sml, smr)
+
+    def all_prod(self) -> typing.Any:
+        return self._d[1]
+
+    def apply(
+        self,
+        left: int,
+        right: typing.Optional[int] = None,
+        f: typing.Optional[typing.Any] = None,
+    ) -> None:
+        assert f is not None
+
+        if right is None:
+            p = left
+            assert 0 <= left < self._n
+
+            p += self._size
+            for i in range(self._log, 0, -1):
+                self._push(p >> i)
+            self._d[p] = self._mapping(f, self._d[p])
+            for i in range(1, self._log + 1):
+                self._update(p >> i)
+        else:
+            assert 0 <= left <= right <= self._n
+            if left == right:
+                return
+
+            left += self._size
+            right += self._size
+
+            for i in range(self._log, 0, -1):
+                if ((left >> i) << i) != left:
+                    self._push(left >> i)
+                if ((right >> i) << i) != right:
+                    self._push((right - 1) >> i)
+
+            l = left
+            r = right
+            while left < right:
+                if left & 1:
+                    self._all_apply(left, f)
+                    left += 1
+                if right & 1:
+                    right -= 1
+                    self._all_apply(right, f)
+                left >>= 1
+                right >>= 1
+            left = l
+            right = r
+
+            for i in range(1, self._log + 1):
+                if ((left >> i) << i) != left:
+                    self._update(left >> i)
+                if ((right >> i) << i) != right:
+                    self._update((right - 1) >> i)
+
+    def max_right(self, left: int, g: typing.Callable[[typing.Any], bool]) -> int:
+        assert 0 <= left <= self._n
+        assert g(self._e)
+
+        if left == self._n:
+            return self._n
+
+        left += self._size
+        for i in range(self._log, 0, -1):
+            self._push(left >> i)
+
+        sm = self._e
+        first = True
+        while first or (left & -left) != left:
+            first = False
+            while left % 2 == 0:
+                left >>= 1
+            if not g(self._op(sm, self._d[left])):
+                while left < self._size:
+                    self._push(left)
+                    left *= 2
+                    if g(self._op(sm, self._d[left])):
+                        sm = self._op(sm, self._d[left])
+                        left += 1
+                return left - self._size
+            sm = self._op(sm, self._d[left])
+            left += 1
+
+        return self._n
+
+    def min_left(self, right: int, g: typing.Any) -> int:
+        assert 0 <= right <= self._n
+        assert g(self._e)
+
+        if right == 0:
+            return 0
+
+        right += self._size
+        for i in range(self._log, 0, -1):
+            self._push((right - 1) >> i)
+
+        sm = self._e
+        first = True
+        while first or (right & -right) != right:
+            first = False
+            right -= 1
+            while right > 1 and right % 2:
+                right >>= 1
+            if not g(self._op(self._d[right], sm)):
+                while right < self._size:
+                    self._push(right)
+                    right = 2 * right + 1
+                    if g(self._op(self._d[right], sm)):
+                        sm = self._op(self._d[right], sm)
+                        right -= 1
+                return right + 1 - self._size
+            sm = self._op(self._d[right], sm)
+
+        return 0
+
+    def _update(self, k: int) -> None:
+        self._d[k] = self._op(self._d[2 * k], self._d[2 * k + 1])
+
+    def _all_apply(self, k: int, f: typing.Any) -> None:
+        self._d[k] = self._mapping(f, self._d[k])
+        if k < self._size:
+            self._lz[k] = self._composition(f, self._lz[k])
+
+    def _push(self, k: int) -> None:
+        self._all_apply(2 * k, self._lz[k])
+        self._all_apply(2 * k + 1, self._lz[k])
+        self._lz[k] = self._id
+
+
+def helltractor():
+    ac, dr = MII()
+    n = II()
+    a = LII()
+    b = LII()
+    m = II()
+    c = [LII() for _ in range(m)]
+
+    mx = 2 * 10**6 + 1
+    f = lambda x, y: fmax(0, x - ac) + fmax(0, y - dr)    
+    p = [f(x, y) for x, y in zip(a, b)]
+    cnt = [-i for i in range(mx)]
+    sg = LazySegmentTree(fmin, 0, add, add, 0, cnt)
+    for x in p:
+        sg.apply(x + 1, mx, 1)
+    
+    pre = -1
+    for k, x, y in c:
+        res = -1
+        if f(x, y) != p[k - 1]:
+            sg.apply(p[k - 1] + 1, mx, -1)
+            p[k - 1] = f(x, y)
+            sg.apply(p[k - 1] + 1, mx, 1)
+        else:
+            res = pre
+        if res == -1:
+            res = sg.max_right(0, lambda v: v >= 0) - 1
+        print(res)
+
+
+if __name__ == "__main__":
+    helltractor()
